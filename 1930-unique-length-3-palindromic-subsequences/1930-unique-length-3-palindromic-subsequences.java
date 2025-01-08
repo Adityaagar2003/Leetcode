0662483dc82ce4732de612e2a1d6ff5462a4b628
Aditya Agarwal <136118380+Adityaagar2003@users.noreply.github.com>
2025-01-08 19:04:26 +0530

    Time: 247 ms (24.2%), Space: 45.6 MB (41.14%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,32 +1,40 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-         int n = s.length();
-        
-        // STEP-1: Get first & last occurrences of each char
-        Map<Character, int[]> firstLast = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            char c = s.charAt(i);
-            if (!firstLast.containsKey(c)) {
-                firstLast.put(c, new int[]{i, i});
-            } else {
-                firstLast.get(c)[1] = i;
-            }
+        int n = s.length();
+        Set<Character> uniqueLetters = new HashSet<>();
+
+        for (char ch : s.toCharArray()) {
+            uniqueLetters.add(ch);
         }
-        
-        // STEP-2: Find unique elements count in range
-        int count = 0;
-        for (Map.Entry<Character, int[]> entry : firstLast.entrySet()) {
-            int[] range = entry.getValue();
-            if (range[0] == range[1]) continue; // No range to consider
-            
-            Set<Character> uniqueChars = new HashSet<>();
-            for (int i = range[0] + 1; i < range[1]; ++i) {
-                uniqueChars.add(s.charAt(i));
+
+        int result = 0;
+
+        for (char letter : uniqueLetters) {
+
+            int firstIdx = -1;
+            int lastIdx = -1;
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == letter) {
+
+                    if (firstIdx == -1) {
+                        firstIdx = i;
+                    }
+
+                    lastIdx = i;
+
+                }
             }
-            count += uniqueChars.size();
+
+            Set<Character> set = new HashSet<>();
+            for (int middle = firstIdx + 1; middle <= lastIdx - 1; middle++) {
+                set.add(s.charAt(middle));
+            }
+
+            result += set.size();
+
         }
-        
-        return count;
-        
+
+        return result;
     }
 }
